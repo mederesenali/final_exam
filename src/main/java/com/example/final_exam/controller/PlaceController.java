60781037a666142eccb076c493cbb7eeea79b558
Medermeda <kairullaevmederbek@gmail.com>
2020-08-15 10:36:27 +0600

    создал контроллер и скрипт в миграциях

---
+++ src/main/java/com/example/final_exam/controller/PlaceController.java
@@ -0,0 +1,9 @@
+package com.example.final_exam.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PlaceController {
+
+
+}
---
+++ src/main/resources/db/migration/V2__create_place.sql
@@ -0,0 +1,8 @@
+create table place(
+ id int not null auto_increment,
+ tag varchar(45) not null ,
+ description varchar(200) ,
+ filename varchar(500),
+ primary key (id)
+
+)
